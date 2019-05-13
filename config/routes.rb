eb74ceb97e926c01de5bd4daf5440c1b79804471
pradeepruhil85 <pradeepruhil85@gmail.com>
2019-05-13 08:26:08 +0530

    Initial commit

---
+++ config/routes.rb
@@ -0,0 +1,11 @@
+Rails.application.routes.draw do
+
+  root "pages#home"  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  get "/about", to: "pages#about"
+
+  get "/help", to: "pages#help"
+
+  resources :todos
+
+end
